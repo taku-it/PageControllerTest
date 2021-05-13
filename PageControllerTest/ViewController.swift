9f987adf32379903d570b6cc11456bfe4a41b68b
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2021-05-13 20:26:22 +0900

    PageViewControllerのテスト

--- PageControllerTest/ViewController.swift
+++ PageControllerTest/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIPageViewController {
 
+    var pageViewControllers: [UIViewController] = []
+    var nowPage: Int = 0
+    
+    var currentPage: Int = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        delegate = self
+        dataSource = self
+        
+        let firstView = storyboard?.instantiateViewController(identifier: "FirstVC") as! FirstViewController
+        let secondView = storyboard?.instantiateViewController(identifier: "SecondVC") as! SecondViewController
+        let thirdView = storyboard?.instantiateViewController(identifier: "ThirdVC") as! ThirdViewController
+        pageViewControllers = [firstView, secondView, thirdView]
+        
+        self.setViewControllers([pageViewControllers[0]], direction: .forward, animated: true, completion: nil)
+       
     }
+    
 
-
+  
 }
 
+extension ViewController: UIPageViewControllerDelegate{
+    
+}
+
+extension ViewController: UIPageViewControllerDataSource{
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        let index = pageViewControllers.firstIndex(of: viewController)
+        if index == 0 {
+            return nil
+        }else{
+            return pageViewControllers[index! - 1]
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let index = pageViewControllers.firstIndex(of: viewController)
+        if index == pageViewControllers.count - 1{
+            return nil
+        }else{
+            return pageViewControllers[index! + 1]
+        }
+        
+    }
+    
+    //    全ページ返すメソッド
+        func presentationCount(for pageViewController: UIPageViewController) -> Int {
+            return pageViewControllers.count
+        }
+        
+    //    現在のページを返すメソッド
+        func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+            return currentPage
+        }
+    
+    
+    
+}

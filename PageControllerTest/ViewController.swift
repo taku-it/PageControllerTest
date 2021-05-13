6bbed1be9dbfb44e3c1a49e01ae0a8462f187e82
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2021-05-13 18:59:10 +0900

    Initial Commit

---
+++ PageControllerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PageControllerTest
+//
+//  Created by 生田拓登 on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
